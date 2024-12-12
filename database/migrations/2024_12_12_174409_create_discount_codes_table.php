f44885534ff03e75be61e33011cb93baa9ce2b83
T-Clement <toquet.clement@gmail.com>
2024-12-12 18:50:09 +0100

    add migration for discount code and handling discount code in orders table

---
+++ database/migrations/2024_12_12_174409_create_discount_codes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->enum('discount_type', ['percentage', 'fixed']);
+            $table->integer('discount_value');
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('begin_at');
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount_codes');
+    }
+};
---
+++ database/migrations/2024_12_12_174749_add_discount_code_to_orders.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('discount_code_applied')->nullable();
+            $table->integer('discount_applied_in_cents')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
